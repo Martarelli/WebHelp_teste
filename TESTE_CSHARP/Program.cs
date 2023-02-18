46b7e416bfea3ef37b7a3cf7c2e883117ad6a447
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2023-02-18 20:24:59 -0300

    exercicio proposto resolvido

--- TESTE_CSHARP/Program.cs
+++ TESTE_CSHARP/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+long maiorSequencia = 1;
+
+for (long i = 1; i <= 1000000; i++)
+{
+    long numero = i;
+    long count = 1;
+    while (numero != 1)
+    {
+        if (numero % 2 == 0)
+        {
+            numero = numero / 2;
+            count++;
+        }
+        else
+        {
+            numero = numero * 3 + 1;
+            count++;
+        }
+    }
+    
+    if (count > maiorSequencia)
+    {
+       maiorSequencia = i; 
+    }
+    
+}
+System.Console.WriteLine("O número que produz a maior sequencia é: " + maiorSequencia);
